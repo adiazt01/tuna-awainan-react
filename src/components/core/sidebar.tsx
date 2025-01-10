5d6c1fa04ebc9407b34f9397ed6d844d9698835a
adiazt01 <adiaztovar2004@gmail.com>
2025-01-10 19:36:23 -0400

    add sidebar link to Home; update button styles for sidebar use

--- src/components/core/sidebar.tsx
+++ src/components/core/sidebar.tsx
@@ -5,6 +5,8 @@ import {
     SidebarGroup,
     SidebarHeader,
   } from "@/components/ui/sidebar"
+import { Button } from "../ui/button"
+import { Link } from "@tanstack/react-router"
    
   export function AppSidebar() {
     return (
@@ -12,7 +14,12 @@ import {
         <SidebarHeader />
         <SidebarContent className="">
           <SidebarGroup />
-          <SidebarGroup />
+          <SidebarGroup >
+            <Button asChild variant={"sidebarLink"} size={"sidebarLink"}>
+                <Link to="/">Home</Link>
+            </Button>
+        </SidebarGroup>
+
         </SidebarContent>
         <SidebarFooter />
       </Sidebar>
