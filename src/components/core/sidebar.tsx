7153c55b8813e4c71d2b568ab5a2ebc2e412632f
adiazt01 <adiaztovar2004@gmail.com>
2025-01-10 19:17:46 -0400

    add sidebar component, skeleton loader, and mobile hook; update Tailwind config and package dependencies

---
+++ src/components/core/sidebar.tsx
@@ -0,0 +1,20 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+  } from "@/components/ui/sidebar"
+   
+  export function AppSidebar() {
+    return (
+      <Sidebar >
+        <SidebarHeader />
+        <SidebarContent className="">
+          <SidebarGroup />
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
+    )
+  }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/core/sidebar'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,27 +11,14 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>{' '}
-        <Link
-          to="/about"
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+      <SidebarProvider>
+        <AppSidebar />
+        <main className='border border-red-500 flex-1'>
+          {/* <SidebarTrigger /> */}
+
+          <Outlet />
+        </main>
+      </SidebarProvider>
       <TanStackRouterDevtools position="bottom-right" />
     </>
   )
